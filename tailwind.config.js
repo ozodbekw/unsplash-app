13714d87c28b3fb2bc5781a1d42eedcc96036757
Ozodbek Tursunaliyev <ozodbektursunaliyev11@gmail.com>
2024-12-12 16:45:06 +0500

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        primary: colors.blue,
-      },
-    },
-  },
+  theme: {},
   plugins: [require("daisyui")],
   daisyui: {
     themes: ["dracula", "winter"],
