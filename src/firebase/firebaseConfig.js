e56d9c7c784069dc47c9975cd77c8c23672b18d3
Ozodbek Tursunaliyev <ozodbektursunaliyev11@gmail.com>
2025-01-04 14:39:36 +0500

    feat: update Firebase configuration to use environment variables

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCq0203zwxbEMUJTXdGlGnb9B59QKjrhK0",
-  authDomain: "my-splash-8137f.firebaseapp.com",
-  projectId: "my-splash-8137f",
-  storageBucket: "my-splash-8137f.firebasestorage.app",
-  messagingSenderId: "1055260867137",
-  appId: "1:1055260867137:web:692e4f53b62cbb5e6024c0",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
