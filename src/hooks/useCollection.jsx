38fb1b921048c0b3070baec2d7ee36e19bd67868
Ozodbek Tursunaliyev <ozodbektursunaliyev11@gmail.com>
2025-01-17 22:41:42 +0500

    feat: update useCollection hook to support conditional queries and modify GlobalContext to filter liked images by user ID

--- src/hooks/useCollection.jsx
+++ src/hooks/useCollection.jsx
@@ -11,18 +11,20 @@ import { db } from "../firebase/firebaseConfig";
 // react imports
 import { useEffect, useState } from "react";
 
-export const useCollection = (collectionName) => {
+export const useCollection = (collectionName, whereData) => {
   const [data, setData] = useState(null);
   useEffect(() => {
-    const q = query(collection(db, collectionName), where("", "==", "CA"));
-    onSnapshot(q, (querySnapshot) => {
-      const queryData = [];
-      querySnapshot.forEach((doc) => {
-        queryData.push({ _id: doc.id, ...doc.data() });
+    if (whereData[2]) {
+      const q = query(collection(db, collectionName), where(...whereData));
+      onSnapshot(q, (querySnapshot) => {
+        const queryData = [];
+        querySnapshot.forEach((doc) => {
+          queryData.push({ _id: doc.id, ...doc.data() });
+        });
+        setData(queryData);
       });
-      setData(queryData);
-    });
-  }, []);
+    }
+  }, [whereData[2]]);
 
   return { data };
 };
